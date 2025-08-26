46a3864732b810213111410f34610fa5c8bea418
atomicGandi <lore.ruelasg@gmail.com>
2025-08-26 07:52:01 -0600

    Se quito variable no usada

--- random_hash.py
+++ random_hash.py
@@ -3,10 +3,11 @@ import random
 
 def find00_in1000Attempts():
     """Intenta encontrar un hash que empiece con '00' en 1000 intentos."""
-    for attempt in range(0, 1000):
+    for _ in range(1000):
         hash_val = ''.join(
             random.choice('0123456789abcdef') for _ in range(32)
         )
         if hash_val.startswith("00"):
             return True
+
     return False
